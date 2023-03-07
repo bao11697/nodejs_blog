3971322af6aca9886670daefd8877db13e878870
Bao.le <bao.le@discoveryloft.com>
2023-03-08 00:15:28 +0700

    separate module to writing code easy

--- src/routes/site.js
+++ src/routes/site.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const newsController = require('../app/controllers/NewController');
+const siteController = require('../app/controllers/SiteController');
 
 // newsController.index
 
-router.use('/:slug',newsController.show);
-router.use('/',newsController.index);
+router.use('/search',siteController.search);
+router.use('/',siteController.home);
 
 module.exports = router;
